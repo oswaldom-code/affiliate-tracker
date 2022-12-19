d635d11f28283b18bd7ca82e71bb405d9422534c
Oswaldo Montaño <oswaldoeliezer876@gmail.com>
2022-12-19 17:18:13 +0100

    ✨ Feat(services) Add support for querying records in the DB
        -  [adapter/http] The handlers are added to handle requests for que-
           rying records in the database.
        -  [adapter/repository] Null pointer to connection pool error is
           fixed.
       -  [adapter/repository] Functions are added to query the database.

--- src/adapters/http/rest/handlers/affiliate_tracker.handlers.go
+++ src/adapters/http/rest/handlers/affiliate_tracker.handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/oswaldom-code/affiliate-tracker/src/adapters/http/rest/dto"
@@ -33,7 +35,7 @@ func (h *Handler) GetReferralLink(c *gin.Context) {
 			})
 		return
 	}
-	// response format
+	// response
 	c.JSON(http.StatusOK, &dto.Response{
 		Success: true,
 		Message: "ID generated successfully",
@@ -70,4 +72,99 @@ func (h *Handler) ProcessRequest(c *gin.Context) {
 	}
 	// redirect to job
 	c.Redirect(http.StatusMovedPermanently, url)
+}
+
+func (h *Handler) ReferredGetAll(c *gin.Context) {
+	// dependency injection AffiliateTrackerServices
+	affiliateTrackerServices := AffiliateTrackerServices.NewTrackerService()
+
+	referredList, err := affiliateTrackerServices.GetAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,
+			dto.Response{
+				Success: false,
+				Message: "Oops at this time we can not process requests, please try again later",
+				Error:   err.Error(),
+			})
+		return
+	}
+
+	var message string
+	if len(referredList) == 0 {
+		message = "There are no records to display"
+	} else {
+		message = fmt.Sprintf("There are %v records to display", len(referredList))
+	}
+	// response format
+	c.JSON(http.StatusOK, &dto.Response{
+		Success: true,
+		Message: message,
+		Data:    referredList,
+	})
+}
+
+func (h *Handler) ReferredGetById(c *gin.Context) {
+	// dependency injection AffiliateTrackerServices
+	affiliateTrackerServices := AffiliateTrackerServices.NewTrackerService()
+	// get parameters from request
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest,
+			dto.Response{
+				Success: false,
+				Message: "Something has gone wrong, please check the request parameters",
+				Error:   err.Error(),
+			})
+		return
+	}
+	referred, err := affiliateTrackerServices.GetById(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound,
+			dto.Response{
+				Success: false,
+				Message: "Oops at this time we can not process requests, please try again later",
+				Error:   err.Error(),
+			})
+		return
+	}
+
+	// response format
+	c.JSON(http.StatusFound, &dto.Response{
+		Success: true,
+		Message: "Query completed successfully",
+		Data:    referred,
+	})
+}
+
+func (h *Handler) ReferredGetByAgentId(c *gin.Context) {
+	// dependency injection AffiliateTrackerServices
+	affiliateTrackerServices := AffiliateTrackerServices.NewTrackerService()
+	// get parameters from request
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest,
+			dto.Response{
+				Success: false,
+				Message: "Something has gone wrong, please check the request parameters",
+				Error:   "The id parameter is required",
+			})
+		return
+	}
+	referred, err := affiliateTrackerServices.GetByAgentId(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound,
+			dto.Response{
+				Success: false,
+				Message: "Oops at this time we can not process requests, please try again later",
+				Error:   err.Error(),
+			})
+		return
+	}
+
+	// response format
+	c.JSON(http.StatusFound, &dto.Response{
+		Success: true,
+		Message: "Query completed successfully",
+		Data:    referred,
+	})
 }
--- src/services/affiliate_tracker_services/types/types.go
+++ src/services/affiliate_tracker_services/types/types.go
@@ -1,6 +1,12 @@
 package types
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/oswaldom-code/affiliate-tracker/src/domain/models"
+)
 
 type ReferralLink struct {
 	Agent string `json:"agent"`
@@ -11,12 +17,43 @@ func (r *ReferralLink) ToString() string {
 	return fmt.Sprintf("%s:::%s", r.Agent, r.Url)
 }
 
-type Referred struct {
-	Id               int64
-	Agent            string `json:"agent"`
-	Url              string `json:"url"`
-	RequestReferer   string `json:"referer"`
-	RequestIp        string `json:"ip"`
-	RequestUserAgent string `json:"user_agent"`
-	CreatedAt        string `json:"cread_at"`
+type ReferredDTO struct {
+	Id               int64     `json:"id"`
+	AgentId          string    `json:"agent"`
+	JobUrl           string    `json:"url"`
+	RequestReferer   string    `json:"referer"`
+	RequestIp        string    `json:"ip"`
+	RequestUserAgent string    `json:"user_agent"`
+	CreatedAt        time.Time `json:"cread_at"`
+}
+
+func (r *ReferredDTO) ToModel() models.Referred {
+	return models.Referred{
+		AgentId:          strings.ToUpper(r.AgentId),
+		JobUrl:           r.JobUrl,
+		RequestReferer:   r.RequestReferer,
+		RequestIp:        r.RequestIp,
+		RequestUserAgent: r.RequestUserAgent,
+		CreatedAt:        time.Now(),
+	}
+}
+
+func (r *ReferredDTO) ReferredModelToReferredDTO(referred models.Referred) {
+	r.Id = referred.ID
+	r.AgentId = referred.AgentId
+	r.JobUrl = referred.JobUrl
+	r.RequestReferer = referred.RequestReferer
+	r.RequestIp = referred.RequestIp
+	r.RequestUserAgent = referred.RequestUserAgent
+	r.CreatedAt = referred.CreatedAt
+}
+
+func (r *ReferredDTO) ReferredModelsToReferredDTOs(referredList []models.Referred) []ReferredDTO {
+	var referredDTOList []ReferredDTO
+	for _, referred := range referredList {
+		referredDTO := ReferredDTO{}
+		referredDTO.ReferredModelToReferredDTO(referred)
+		referredDTOList = append(referredDTOList, referredDTO)
+	}
+	return referredDTOList
 }
